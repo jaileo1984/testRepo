09435f52f594c6473544e072ecf896bc75635f53
Jaivardhan Singh <Jaivardhan.Singh@synapse.com>
2017-05-25 20:25:51 +0530

    jai committed new change

--- src/main/java/foo/App.java
+++ src/main/java/foo/App.java
@@ -47,7 +47,7 @@ public int  deleteAccount(int a,int b){
 
     public static void main( String[] args )
     {
-        System.out.println( "Hello again to newer  the world12!" );
+        System.out.println( "Hello again to newer  the world!" );
         
     }
 }
